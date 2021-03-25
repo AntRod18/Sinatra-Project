361a939b7ab5bec550d05a1e9ccaecb3da70bd67
AntRod18 <antrod1895@gmail.com>
2021-03-25 18:03:07 -0400

    added follows table and created follow model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_many :posts
   has_secure_password
   validates :username, presence: true
+
+  has_many :follows
+  has_many :follower_relationships, foreign_key: :following_id, class_name: "Follow"
+  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :follower_relationships, foreign_key: :user_id, class_name: "Follow"
+  has_many :following, through: :following_relationships, source: :following
   end
