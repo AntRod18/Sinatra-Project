6c41e5e0109d7111d9f68cec161f62b7f3d9adba
AntRod18 <antrod1895@gmail.com>
2021-03-26 19:06:14 -0400

    added a navbar

--- db/migrate/20210325214338_follows.rb
+++
@@ -1,9 +0,0 @@
-class Follows < ActiveRecord::Migration[6.0]
-  def change
-    create_table :follows do |t|
-      t.integer :user_id
-      t.integer :following_id
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_214338) do
-
-  create_table "follows", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "following_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_03_24_164737) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
